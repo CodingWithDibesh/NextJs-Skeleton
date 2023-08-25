db5de7b827d8ece9000aefb053ab505e3d346ef2
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-08-25 09:36:13 +0545

    chore: Tested Tailwind on Playground

--- apps/playground/app/page.tsx
+++ apps/playground/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <>Playground App</>;
+  return (
+    <>
+      <div className="flex font-bold text-purple-600 leading-7 justify-center items-center h-11">
+        Playground App
+      </div>
+    </>
+  );
 }
