1ba0e646471d77f93681599c61b30113b7505faa
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-08-25 09:34:17 +0545

    feat: Implemented Tailwind setup on playground

---
+++ apps/playground/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const { tailwindConfig } = require("tailwindcss-config");
+
+module.exports = {
+    ...tailwindConfig,
+    content: ["./**/*.{js,ts,jsx,tsx,mdx}"],
+};
